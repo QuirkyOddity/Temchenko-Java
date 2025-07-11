37022dbbb6c539bc7912cb97ca721fd32493644a
QuirkyOddity <danilatemchenko@yandex.ru>
2025-07-11 20:05:30 +0300

    Create StringComparison.java

---
+++ StringComparison.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class StringComparison {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String a = scanner.nextLine();
+
+        String b = scanner.nextLine();
+
+        if (a.equals(b)) {
+            System.out.println("Строки идентичны");
+        } else {
+            System.out.println("Строки неидентичны");
+        }
+
+        scanner.close();
+    }
+}
